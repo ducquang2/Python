df44e4edfd497ee01ffa2fc76d2efc0b8f6ac16f
ducquang2 <quangbilun@gmail.com>
2020-10-05 17:21:15 +0700

    update

--- Basic_on_HackerRank/List_comprehensions.py
+++ Basic_on_HackerRank/List_comprehensions.py
@@ -3,3 +3,4 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
+print ([[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n ])
