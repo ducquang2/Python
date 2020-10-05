0199753668ba3f88ea8a2e12d17c0eb9c34f590b
ducquang2 <quangbilun@gmail.com>
2020-10-05 17:49:28 +0700

    update

--- Basic_on_HackerRank/Find_the_Runner_up_Score.py
+++ Basic_on_HackerRank/Find_the_Runner_up_Score.py
@@ -1,3 +1,12 @@
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())
+    arr = list(map(int, input().split()))
+    first = 0
+    second = 0
+    for i in range(n):
+        if arr[i] > first:
+            second = first
+            first = arr[i]
+        elif arr[i] > second and arr[i] < first:
+            second = arr[i]
+    print(second)
