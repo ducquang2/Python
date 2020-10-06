1fcea40f21d2f95e3c8ad236f0b79ffa5a84afb3
ducquang2 <quangbilun@gmail.com>
2020-10-06 15:11:02 +0700

    update

--- Basic_on_HackerRank/Nested_Lists.py
+++ Basic_on_HackerRank/Nested_Lists.py
@@ -2,3 +2,8 @@ if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        a.append([score, name])
+
+    a.sort()
+    b = [i for i in a if i[0] != a[0][0]]
+    c = []
