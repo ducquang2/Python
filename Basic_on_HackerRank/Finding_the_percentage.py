83eeeddda98a55455a8b293c500e95c036bbbb08
ducquang2 <quangbilun@gmail.com>
2020-10-09 16:17:42 +0700

    test

---
+++ Basic_on_HackerRank/Finding_the_percentage.py
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+=======
+# Import decimal
+from decimal import Decimal
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    querry_name = input()
+
+# Extract the value into a list: query_scores
+querry_scores = student_marks[querry_name]
+
+# Sum the scores in the list: total_scores
+total_scores = sum(querry_scores)
+
+# Convert the floats to dicimals and average the scores: avg
+avg = Decimal(total_scores/3)
+
+# Print the mean of the scores, correct to two decimals
+print(round(avg,2))
+>>>>>>> repair
