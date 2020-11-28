d6985d02beef7f09a433c6e0c300b5a0c764df86
ducquang2 <quangbilun@gmail.com>
2020-11-28 14:53:52 +0700

    second-way

--- Basic_on_HackerRank/tuples.py
+++ Basic_on_HackerRank/tuples.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
-    t = tuple(int(i) for i in input().split())
+
+    # t = tuple(int(i) for i in input().split())
+    integer_list = map(int, input().split())
+    t = tuple(integer_list)
     print(hash(t))
