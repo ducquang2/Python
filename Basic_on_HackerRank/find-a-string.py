8d0336f8a95cdba5eb0c52329402a706b9cc8520
ducquang2 <quangbilun@gmail.com>
2020-11-28 16:33:07 +0700

    done

---
+++ Basic_on_HackerRank/find-a-string.py
@@ -0,0 +1,14 @@
+def count_substring(string, sub_string):
+    counter = 0
+    for i in range(0, len(string)):
+        if sub_string == string[i:i+len(sub_string)]:
+            counter += 1
+            
+    return counter
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
