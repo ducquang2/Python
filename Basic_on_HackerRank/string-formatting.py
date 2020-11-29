a6d18b281d974d5667f5e17e2e79f418db3bb3d7
ducquang2 <quangbilun@gmail.com>
2020-11-29 10:35:18 +0700

    format-done

--- Basic_on_HackerRank/string-formatting.py
+++ Basic_on_HackerRank/string-formatting.py
@@ -6,11 +6,12 @@ def print_formatted(number):
         octal = str(oct(i)[2:])
         hexa = str(hex(i)[2:]).upper()
         binary = str(bin(i)[2:])
-        formatDecimal = ((" " * (spacePad - len(str(decimal)) - 2)) + decimal) 
-        formatOct = ((" " * (spacePad - len(str(octal)) - 2)) + octal) 
-        formatHex = ((" " * (spacePad - len(str(hexa)) - 2)) + hexa) 
-        formatBin = ((" " * (spacePad - len(str(binary)) - 2)) + binary) 
+        formatDecimal = ((" " * (spacePad - len(str(decimal)) - 2)) + decimal)
+        formatOct = ((" " * (spacePad - len(str(octal)) - 2)) + octal)
+        formatHex = ((" " * (spacePad - len(str(hexa)) - 2)) + hexa)
+        formatBin = ((" " * (spacePad - len(str(binary)) - 2)) + binary)
         print(formatDecimal + " " + formatOct + " " + formatHex + " " + formatBin)
+
 
 if __name__ == '__main__':
     n = int(input())
