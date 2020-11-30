4c4af7a950188c6bfc0ccc4843414d46598f1d7d
ducquang2 <quangbilun@gmail.com>
2020-11-30 23:23:36 +0700

    explain

--- Basic_on_HackerRank/itertools-product.py
+++ Basic_on_HackerRank/itertools-product.py
@@ -5,3 +5,8 @@ a = [int(x) for x in input().split()]
 b = [int(y) for y in input().split()]
 
 print(*product(a, b))
+# Example:
+# a = [1, 3]
+# b = [5, 6]
+# product(a, b) =  [(1, 5), (1, 6), (3, 5), (3, 6)]
+# print(*product(a, b)) -> unpack product(a, b) = print((1, 5), (1, 6), (3, 5), (3, 6))
