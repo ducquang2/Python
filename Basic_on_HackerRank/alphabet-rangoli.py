aa89acc88b88c355404f765de3d76c4be85725dc
ducquang2 <quangbilun@gmail.com>
2020-11-30 23:02:36 +0700

    done

--- Basic_on_HackerRank/alphabet-rangoli.py
+++ Basic_on_HackerRank/alphabet-rangoli.py
@@ -1,5 +1,12 @@
 def print_rangoli(size):
     # your code goes here
+    myStr = 'abcdefghijklmnopqrstuvwxyz'[0:size]
+
+    for i in range(size-1, -size, -1):
+        x = abs(i)
+        line = myStr[size:x:-1] + myStr[x:size]
+        print("--"*x + '-'.join(line) + "--"*x)
+
 
 if __name__ == '__main__':
     n = int(input())
