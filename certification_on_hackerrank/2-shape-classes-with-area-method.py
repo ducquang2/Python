fc2128f44d20be15f10c1689db7df6e0a339d20f
ducquang2 <quangbilun@gmail.com>
2021-07-26 19:45:44 +0700

    add

---
+++ certification_on_hackerrank/2-shape-classes-with-area-method.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+class Rectangle:
+    pass
+
+class Circle:
+    pass
+if __name__ == '__main__':  
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input())
+    queries = []
+    for _ in range(q):
+        args = input().split()
+        shape_name, params = args[0], tuple(map(int, args[1:]))
+        if shape_name == "rectangle":
+            a, b = params[0], params[1]
+            shape = Rectangle(a, b)
+        elif shape_name == "circle":
+            r = params[0]
+            shape = Circle(r)
+        else:
+            raise ValueError("invalid shape type")
+        fptr.write("%.2f\n" % shape.area())
+    fptr.close()
